7016401365a551c53db5c5a94167a95b85f5ce6e
temevh <temehama@gmail.com>
2022-10-31 16:57:14 +0200

    766 init

---
+++ 766ToeplitzMatrix.py
@@ -0,0 +1,8 @@
+def isToeplitzMatrix(matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+
+matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+print(isToeplitzMatrix(matrix))
