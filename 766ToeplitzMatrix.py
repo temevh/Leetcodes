efd1ec357aef9066885ffb515d676f32a339a1c6
temevh <temehama@gmail.com>
2022-10-31 17:21:14 +0200

    working 766

--- 766ToeplitzMatrix.py
+++ 766ToeplitzMatrix.py
@@ -1,8 +1,10 @@
 def isToeplitzMatrix(matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: bool
-        """
+    for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] != matrix[i -1][j - 1]:
+                    return False
+    return True
+
 
 matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
 print(isToeplitzMatrix(matrix))
