90a71021301756d002bd346dcdbacb6b8cdf895a
temevh <temehama@gmail.com>
2022-12-09 16:45:27 +0200

    improved, beats 97%

--- 2011FinalValue.py
+++ 2011FinalValue.py
@@ -1,5 +1,20 @@
-#PASSING, beats 50%, runtime(72ms)
+
+#PASSING, beats 97%, runtime 31ms
 def finalValueAfterOperations(operations):
+    x = 0
+    for oper in operations:
+        if oper == "--X" or oper == "X--":
+            x -= 1
+        else:
+            x += 1
+
+    return x
+
+print(finalValueAfterOperations(["--X","X++","X++"]))
+
+
+#PASSING, beats 50%, runtime(72ms)
+def finalValueAfterOperations2(operations):
     x = 0
     for oper in operations:
         if oper == "--X" or oper == "X--":
@@ -8,5 +23,3 @@ def finalValueAfterOperations(operations):
             x += 1
 
     return x
-
-print(finalValueAfterOperations(["--X","X++","X++"]))
