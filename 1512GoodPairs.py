8650c168d2d7410ec2401887db83e91d9b50920d
temevh <temehama@gmail.com>
2022-12-13 16:35:42 +0200

    first working version

--- 1512GoodPairs.py
+++ 1512GoodPairs.py
@@ -1,4 +1,9 @@
 def numIdenticalPairs(nums):
-    pass
+    res = 0 
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j]:
+                res+=1
+    return res
 
 print(numIdenticalPairs([1,2,3,1,1,3]))
