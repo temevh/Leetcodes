852a384eee839a74e7b50b7435f318eef4f5b87d
temevh <temehama@gmail.com>
2022-12-13 16:24:54 +0200

    passing, beats 96%

--- 1470ShuffleArray.py
+++ 1470ShuffleArray.py
@@ -1,4 +1,11 @@
+#PASSING, beats 96.3% in runtime(40ms)
 def shuffle(nums, n):
-    pass
+    arr = []
+    for i in range(n):
+        arr.append(nums[i])
+        arr.append(nums[i+n])
 
-print(shuffle([2,5,1,3,4,7]))
+    return arr
+
+
+print(shuffle([2,5,1,3,4,7], 3))
