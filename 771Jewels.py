28519cf1b2b1f93abf4d6ee5edbdc395302e4754
temevh <temehama@gmail.com>
2022-12-14 14:54:23 +0200

    working, beats 94% in runtime

--- 771Jewels.py
+++ 771Jewels.py
@@ -1,5 +1,14 @@
+# PASSING, beats 94% in runtime(16ms)
 def numJewelsInStones(jewels, stones):
-    pass
+    jewArr = []
+    matches = 0
+    for letter in jewels:
+        jewArr.append(letter)
+    for letter in stones:
+        if letter in jewArr:
+            matches += 1
+
+    return matches
 
 
-print(numJewelsInStones("aA"))
+print(numJewelsInStones("aA", "aAAbbbb"))
