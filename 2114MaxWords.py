db43cb639686172da79f54c995496811463b1f7d
temevh <temehama@gmail.com>
2022-12-15 15:27:15 +0200

    2114 init

---
+++ 2114MaxWords.py
@@ -0,0 +1,8 @@
+def mostWordsFound(sentences):
+    pass
+
+
+sentences = ["alice and bob love leetcode",
+             "i think so too", "this is great thanks very much"]
+
+print(mostWordsFound(sentences))
