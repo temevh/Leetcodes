2be92119984653eba03343a42a620ecc70a03259
temevh <temehama@gmail.com>
2022-12-26 11:10:28 +0200

    first test

--- 1365SmallerThan.py
+++ 1365SmallerThan.py
@@ -1,4 +1,14 @@
 def smallerNumbersThanCurrent(nums):
-    pass
+    arr =[0] * len(nums)
+    ctr = 0
+    for i in range(len(nums)):
+        for j in nums:
+            if j < nums[i]:
+                ctr += 1
+        arr[i] = ctr
+        ctr = 0
+    return arr
+
+            
 
 print(smallerNumbersThanCurrent([8,1,2,2,3]))
