1301dee3a335a63067d68a751cacd1548931ecd0
temevh <temehama@gmail.com>
2024-02-14 16:10:49 +0200

    2149 working

--- 2149RearrangeArray.py
+++ 2149RearrangeArray.py
@@ -1,7 +1,31 @@
-class Solution(object):
-    def rearrangeArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        
+
+def rearrangeArray(nums):
+    toReturn = []
+    pos = []
+    neg = []
+    #1. Joka toinen pos ja neg
+    #2. Samanmerkkisillä järjestys säilyy
+    #3. Uusi array alkaa positiivisella
+    for num in nums:
+        if (num < 0):
+            neg.append(num)
+        else:
+            pos.append(num)
+
+    count, negCount, posCount = 0, 0, 0
+    for num in nums:
+        if count % 2 == 0:
+            n = pos[posCount]
+            toReturn.append(n)
+            posCount += 1
+        else:
+            n = neg[negCount]
+            toReturn.append(n)
+            negCount += 1
+        count += 1
+
+    return toReturn
+
+
+nums = [3,1,-2,-5,2,-4] 
+print(rearrangeArray(nums)) # Tulisi olla [3,-2,1,-5,2,-4]
