3ccf5d8251df4d31771e7abd1dbd0d17f224f80c
temevh <temehama@gmail.com>
2024-02-15 15:55:26 +0200

    added return clauses and task comment

--- 2971Polygon.py
+++ 2971Polygon.py
@@ -1,8 +1,20 @@
+#Valitse numsista ne 3 numeroa joiden summa on mahdollisimman lähellä (mutta pienempi) kuin maximi
 def largestPerimeter(nums):
+    perimeter = 0
+    if (len(nums) < 3):
+        print("size return")
+        return -1
     maxSide = max(nums)
     print("max side", maxSide)
+    if((sum(nums) - maxSide) < maxSide):
+        print("sum return")
+        return -1
 
-    return -1
+
+
+
+    return perimeter
+
 
 
 nums = [5,5,5]
