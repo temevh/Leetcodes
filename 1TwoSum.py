c8bbd884a1ea82565c15ba84693fe359dcf9353a
temevh <temehama@gmail.com>
2024-02-15 17:11:15 +0200

    fixed brute-force

--- 1TwoSum.py
+++ 1TwoSum.py
@@ -1,16 +1,14 @@
 def twoSum(nums, target):
     result = [0,0]
-    nums.sort()
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            if(nums[i] + nums[j] == target):
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if (nums[i] + nums[j] == target):
                 result[0] = i
                 result[1] = j
                 return result
+    return result     
 
-    return result
 
-
-nums = [2, 7, 11, 15]
-target = 9
+nums = [3,2,4]
+target = 6
 print(twoSum(nums, target))
