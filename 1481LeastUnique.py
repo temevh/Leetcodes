f0fa19d3052e66317a72266b6b248363d1b246af
temevh <temehama@gmail.com>
2024-02-16 13:19:24 +0200

    added dict

--- 1481LeastUnique.py
+++ 1481LeastUnique.py
@@ -1,8 +1,17 @@
 #arraysta poistetaan k verran elementtejä, mikä on pienin mahdollinen määrä uniikkeja elementtejä
-# (eli maksimimäärä duplikateja)
-
+# (eli maksimimäärä duplikatejag)
+#Dictionary jossa jokainen numero ja määrä -> poistetaan vaan k "harvinaisimmat"
 
 def findLeastNumOfUniqueInts(arr, k):
+    dict = {}
+    for i in range(len(arr)):
+        if (arr[i] in dict):
+            dict[arr[i]] += 1
+        else:
+            dict[arr[i]] = 1
+
+    print(dict)
+
     amount = 0
     return amount
 
