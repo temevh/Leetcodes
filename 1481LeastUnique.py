68f9f44f6ff51d2aa355c537d258ab9a621bc738
temevh <temehama@gmail.com>
2024-02-16 13:12:37 +0200

    1481 task added

--- 1481LeastUnique.py
+++ 1481LeastUnique.py
@@ -1,3 +1,7 @@
+#arraysta poistetaan k verran elementtejä, mikä on pienin mahdollinen määrä uniikkeja elementtejä
+# (eli maksimimäärä duplikateja)
+
+
 def findLeastNumOfUniqueInts(arr, k):
     amount = 0
     return amount
