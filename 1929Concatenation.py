30a6acd5d88f783813ba3d380ae776b1b0fec646
temevh <temehama@gmail.com>
2024-02-19 16:48:27 +0200

    a working simple solution

--- 1929Concatenation.py
+++ 1929Concatenation.py
@@ -1,25 +1,12 @@
-
-# PASSING, runtime 69ms, beats(85%)
 def getConcatenation(nums):
-    toReturn = [0] * (2 * len(nums))
+    result = [None] * 2*len(nums)
 
-    index = 0
-    for i in nums:
-        toReturn[index] = i
-        toReturn[index+len(nums)] = i
-        index += 1
-    return toReturn
+    for i in range(len(nums)):
+        result[i] = nums[i]
+        result[i+len(nums)] = nums[i]
+
+    return result
 
 
-nums = [1, 2, 1]
+nums = [1,2,1]
 print(getConcatenation(nums))
-
-
-# PASSING, runtime 155ms (beats 31%)
-def getConcatenation2(nums):
-    toReturn = []
-    for i in nums:
-        toReturn.append(i)
-    for i in nums:
-        toReturn.append(i)
-    return toReturn
